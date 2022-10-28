60f4dffc26e6f51a36ca37d32467ab0c04f4e015
maxCD <maxCD@LAPTOP-NOVROJU5>
2022-10-28 14:37:05 +0200

    Debut P2 TP2

--- TP2_convertisseurObjet_DESSORS/src/TP2_convertisseurObjet_DESSORS.java
+++ TP2_convertisseurObjet_DESSORS/src/TP2_convertisseurObjet_DESSORS.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tp2_convertisseurobjet_dessors;
+
 
 /**
  *
@@ -14,6 +14,9 @@ public class TP2_convertisseurObjet_DESSORS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        double res = 0;
+        
+        Convertisseur C1 = new Convertisseur
         // TODO code application logic here
     }
     
