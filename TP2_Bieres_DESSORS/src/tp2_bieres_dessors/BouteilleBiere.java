6a5b35ff05ea686f3d38f74a0d15d3053f33cb05
maxCD <maxCD@LAPTOP-NOVROJU5>
2022-10-30 16:22:49 +0100

    Commentaire sur TP2 Bieress

--- TP2_Bieres_DESSORS/src/tp2_bieres_dessors/BouteilleBiere.java
+++ TP2_Bieres_DESSORS/src/tp2_bieres_dessors/BouteilleBiere.java
@@ -9,24 +9,31 @@ package tp2_bieres_dessors;
  * @author maxCD
  */
 public class BouteilleBiere {
+    //4 lignes permettant de définir les quatre attributs suivants :
     public String Nom;
     public double degreAlcool;
     public String brasserie;
     public boolean ouverte;
-    public BouteilleBiere(String unNom, double unDegre, String
-uneBrasserie) {
- Nom = unNom;
- degreAlcool = unDegre;
- brasserie = uneBrasserie;
- ouverte = false;
+    
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        //Constructeur
+    Nom = unNom;
+    degreAlcool = unDegre;
+    brasserie = uneBrasserie;
+    ouverte = false;
 }
 
 public void lireEtiquette() {
+    //. Cette méthode, lorsqu’elle est appelée, affiche
+    //simplement à l’écran les informations contenues sur l’étiquette de la bouteille de bière. Au sein d’une
+    //méthode, on a accès a la valeur de tous les attributs de la même classe   
 System.out.println("Bouteille de " + Nom +" (" + degreAlcool +
         " degres) \nBrasserie : " + brasserie ) ;
 
 }
 public boolean Décapsuler(){
+    //LA methode qui nous dira si la bouteille est decapsuler ou 
+    //pas en utilisant le methode boolean
         if (ouverte == false){
         ouverte = true;
         return ouverte;
@@ -43,5 +50,7 @@ chaine_a_retourner = Nom + " (" + degreAlcool + " degrés)Ouverte ? ";
 if (ouverte == true ) chaine_a_retourner += "oui" ;
 else chaine_a_retourner += "non" ;
 return chaine_a_retourner ;
+//La méthode toString() permet de faire cela, elle retourne la chaine de
+//caractère que l’on veut afficher quand l’objet est cite
 }
 }
--- TP2_Bieres_DESSORS/src/tp2_bieres_dessors/TP2_Bieres_DESSORS.java
+++ TP2_Bieres_DESSORS/src/tp2_bieres_dessors/TP2_Bieres_DESSORS.java
@@ -17,15 +17,18 @@ public class TP2_Bieres_DESSORS {
         // TODO code application logic here
         BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0 ,"Dubuisson") ;
         uneBiere.ouverte = false;
+        //CREATION de la nouvelle bouteille de biere dubuisson avec comme variable BouteilleBiere
         uneBiere.lireEtiquette();
         BouteilleBiere autreBiere = new BouteilleBiere("Abbaye de Leffe" ,6.6 ,"Leffe");
         autreBiere.ouverte = false;
+        //Creation de bierre leffe
         autreBiere.lireEtiquette();
         BouteilleBiere tequBiere = new BouteilleBiere ("Campo de AGAVE Tequila" , 40.0, "HerraduraBIERE");
         tequBiere.ouverte = true ;
+        //Creation de biere personelle HEARRADURABierre
         tequBiere.lireEtiquette();
         System.out.println(uneBiere) ;
-
+        //Unsystem out print pour nous afficher chaque bierre et ses details
     }
 
 
