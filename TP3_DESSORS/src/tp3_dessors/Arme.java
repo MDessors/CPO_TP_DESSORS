337614670f5ba8a095f7e2bcbf79107b5c3f3d26
maxCD <maxCD@LAPTOP-NOVROJU5>
2022-11-14 09:42:34 +0100

    TP3 Creation class Epée et Baton

---
+++ TP3_DESSORS/src/tp3_dessors/Arme.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_dessors;
+
+/**
+ *
+ * @author maxCD
+ */
+public class Arme {
+    String nom;
+    private int nivDatack ;
+    
+public String toString(){
+    return ("Nom de l'arme:"+ nom + "\Niveau de l'arme:"+nivDatack) ;
+}
+}
